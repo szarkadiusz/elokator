67e98122b70e3ed6f6875ecb93d18d446825e541
Arek Szymanski <arkadiuszsz@gmail.com>
2020-08-30 22:50:42 +0200

    Initial commit

---
+++ src/main/java/online/elokator/MainController.java
@@ -0,0 +1,7 @@
+package online.elokator;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+}
---
+++ src/test/java/online/elokator/ElokatorApplicationTests.java
@@ -0,0 +1,13 @@
+package online.elokator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElokatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
