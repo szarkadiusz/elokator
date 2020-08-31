88fa506334294ade8ad6ffd56a69774444caa894
Arek Szymanski <arkadiuszsz@gmail.com>
2020-08-31 23:18:17 +0200

    changes - fight with Spring problems

--- src/main/java/online/elokator/MainController.java
+++ src/main/java/online/elokator/MainController.java
@@ -3,12 +3,12 @@ package online.elokator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
 public class MainController {
 
-   @GetMapping("/home")
-    private String home(){
-        return null;
-    }
+
 }
 //https://spring.io/guides/gs/serving-web-content/#initial
+//https://www.javatpoint.com/spring-mvc-multiple-view-page-example
