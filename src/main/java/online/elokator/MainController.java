c92a1b2c73d116182c7ad9023da5e2819a87a641
Arek Szymanski <arkadiuszsz@gmail.com>
2020-09-05 09:00:12 +0200

    changes - fight with Spring problems

--- src/main/java/online/elokator/MainController.java
+++ src/main/java/online/elokator/MainController.java
@@ -1,14 +1,24 @@
 package online.elokator;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class MainController {
 
 
+    @RequestMapping("/hello")
+    public String redirect() {
+        return "viewpage";
+    }
+
+    @RequestMapping("/helloagain")
+    public String display() {
+        return "final";
+    }
 }
+
 //https://spring.io/guides/gs/serving-web-content/#initial
 //https://www.javatpoint.com/spring-mvc-multiple-view-page-example
